fc0c896c6fe6cb4e7ab44e3255b81364a68983e4
Gerry Said <gerrysaid@gmail.com>
2014-03-25 14:20:18 +0100

    Created a simple rocketship object
    Created a simple rocketship object.  Video tutorial of the lesson will
    be posted to youtube soon.

---
+++ Assets/Scripts/rocketshipController.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+//declaring an integer (number) variable which will control rocket speed
+var rocketSpeed:int;
+
+//runs ONCE at the beginning of the game
+function Start () {
+	
+}
+
+
+function move(rSpeed:int)
+{
+	//x axis -1 / 0 / 1
+	//transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal"))
+	if (Input.GetKey(KeyCode.LeftArrow))
+	{
+		transform.Translate(Vector3.left * Time.deltaTime * rSpeed);
+	}
+	
+	if (Input.GetKey(KeyCode.RightArrow))
+	{
+		transform.Translate(Vector3.right * Time.deltaTime * rSpeed);
+	}
+	
+	//to implement the up and down
+	if (Input.GetKey(KeyCode.UpArrow))
+	{
+		transform.Translate(Vector3.up * Time.deltaTime * rSpeed);
+	}
+	
+	if (Input.GetKey(KeyCode.DownArrow))
+	{
+		transform.Translate(Vector3.down * Time.deltaTime * rSpeed);
+	}
+}
+
+//runs ALL THE TIME while the game is running (when play button is pressed)
+function Update () {
+	
+	move(rocketSpeed);
+		
+}
